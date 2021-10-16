f5d264e9099f71961af67652bce5e3f872e8f737
Prakash Raman <prakash.raman@sentiance.com>
2021-10-16 22:24:19 +0200

    node codedoc

--- api/app.ts
+++ api/app.ts
@@ -1,9 +1,18 @@
+/**
+ * Express router sets up the debug endpoints and socketio server
+ * @module index/routes
+ */
+
 import express from "express";
 import cors from "cors";
 
 import { IO } from "./lib/socket";
 import { logger } from "./lib/init";
 
+/**
+ * Express app
+ * @const
+ */
 const app = express();
 
 app.use(
@@ -13,11 +22,23 @@ app.use(
   })
 );
 
+/**
+ * Index
+ * @memberof routes
+ * @function
+ * @name index
+ */
 app.get("/", (_req, res, _next) => {
   res.json({ success: true, now: new Date() });
 });
 
-app.post("/new", (_req, res, _next) => {
+/**
+ * Broadcast a debug message
+ * @memberof debug
+ * @function
+ * @name debug/message
+ */
+app.post("/debug/message", (_req, res, _next) => {
   IO.local.emit("message:new", new Date().toString());
   res.json({
     success: true,
@@ -27,6 +48,7 @@ app.post("/new", (_req, res, _next) => {
 
 /**
  * Starts the REST server
+ * @const
  */
 const server = app.listen(8080, function () {
   logger.info("starting server...", { name: app.name, port: 8080 });
